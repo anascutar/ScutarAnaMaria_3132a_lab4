9fd4f634a504f0f9f0f03f6b570974d4a9daa107
Ana Scutar <katy85276@gmail.com>
2021-11-03 22:39:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Laborator 4- citire coordonate cub din fisier
+
+namespace ScutarAnaMaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Window3D wnd = new Window3D();
+            wnd.Run(30.0, 0.0);
+        }
+    }
+}
---
+++ Punct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScutarAnaMaria
+{
+    class Punct
+    {
+        double x, y, z;
+        public Punct(double xx, double yy, double zz)
+        {
+            x = xx;
+            y = yy;
+            z = zz;
+        }
+    }
+}
